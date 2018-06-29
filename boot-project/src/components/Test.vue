fc92910285c17d48719165bff1a786128094989b
Camile Campos <camilecampos@outlook.com>
2018-06-29 17:21:20 -0300

    vue + bs
    exemplo de aula

---
+++ boot-project/src/components/Test.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <b-alert show>Alerta Padrão</b-alert>
+
+    <b-alert variant="danger"
+             dismissible
+             :show="showDismissibleAlert"
+             @dismissed="showDismissibleAlert=false">
+      Erro! Mensagem de erro...
+    </b-alert>
+    
+    <b-btn @click="showDismissibleAlert=true" variant="info" class="m-1">
+      Apresentar Erro! ({{showDismissibleAlert?'visível':'oculta'}})
+    </b-btn>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'Componente de Teste!!!',
+      showDismissibleAlert: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
